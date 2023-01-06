bd47f4f0ffc0e8b95f3b97087b0701de9c87062a
ed <cabrale234@gmail.com>
2023-01-06 16:05:35 -0500

    completed signup, login, and logout

--- codeAlong/main_app/urls.py
+++ codeAlong/main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('practice/', views.practice, name='practice'),
+    path('practice/<int:leetcode_id>/', views.practice_details, name='detail'),
     path('study/', views.study, name="study"),
-    path('login/', views.login, name="login"),
+    # path('login/', views.login, name="login"),
+    # Django's built-in auth doesn't provide a URL || view for singing up...Gotta make my own.
+    path('accounts/signup/', views.signup, name='signup'),
 ]
